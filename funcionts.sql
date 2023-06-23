18b1653b1fc8f54653d19489ac05863a83c65eef
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-23 14:12:24 +0900

    update

--- funcionts.sql
+++ funcionts.sql
@@ -10,3 +10,15 @@ WHERE id = 3;
 SELECT COUNT(id) FROM items;
 
 SELECT COUNT(id) AS item_count FROM items;
+
+-- 「items」テーブルの「price」の合計金額を計算
+SELECT SUM(price) AS total_price FROM items;
+
+-- 「items」テーブルの「price」の平均金額を計算
+SELECT AVG(price) AS average_price FROM items;
+
+SELECT 
+COUNT(id) AS item_count,
+SUM(price) AS total_price,
+AVG(price) AS average_price
+FROM items;
