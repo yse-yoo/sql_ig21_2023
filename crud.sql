5b11545cde70e68228f936a38ad1844a33b01750
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-23 13:25:55 +0900

    update

--- crud.sql
+++ crud.sql
@@ -7,3 +7,9 @@ INSERT INTO items (code, name, price, stock)
 
 INSERT INTO items (code, name, price, stock) 
     VALUES ("D00003", "ほうじ茶", 400, 100);
+
+-- 「items」テーブルに「id = 1」の在庫数を更新（UPDATE）
+UPDATE items SET stock = 150 WHERE id = 1; 
+
+-- 「items」テーブルに「id = 2」の在庫数を20増やす（UPDATE）
+UPDATE items SET stock = stock + 20 WHERE id = 2; 
