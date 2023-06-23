0b020eec15b4eefb491b296062627fe7fe1f6ac0
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-23 13:11:00 +0900

    add crud.sql

---
+++ crud.sql
@@ -0,0 +1,9 @@
+-- 「items」テーブルにレコード挿入（INSERT）
+INSERT INTO items (code, name, price, stock) 
+    VALUES ("D00001", "コーヒー", 300, 100);
+
+INSERT INTO items (code, name, price, stock) 
+    VALUES ("D00002", "紅茶", 350, 100);
+
+INSERT INTO items (code, name, price, stock) 
+    VALUES ("D00003", "ほうじ茶", 400, 100);
