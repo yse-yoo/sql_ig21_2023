e2f4d753749a26ade520ff6f2cc2bd9743f57c31
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-23 13:33:41 +0900

    update

--- crud.sql
+++ crud.sql
@@ -13,3 +13,10 @@ UPDATE items SET stock = 150 WHERE id = 1;
 
 -- 「items」テーブルに「id = 2」の在庫数を20増やす（UPDATE）
 UPDATE items SET stock = stock + 20 WHERE id = 2; 
+
+-- データ追加
+INSERT INTO items (code, name, price, stock) 
+    VALUES ("D00004", "オレンジジュース", 400, 100);
+
+-- 「items」テーブルから idを指定してレコード削除
+DELETE FROM items WHERE id = 4;
