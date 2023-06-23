cb16d42b43011af8543b89cf5e25e28e776d0d84
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-23 11:21:44 +0900

    update

--- basic.sql
+++ basic.sql
@@ -1,2 +1,14 @@
 -- usersテーブルからすべてのレコードを選択
 SELECT * FROM users;
+
+-- usersテーブルからすべてのレコードを選択（nameとemail）
+SELECT name, email  FROM users;
+
+-- usersテーブルから「id = 1」のレコード選択
+SELECT * FROM users WHERE id = 1;
+
+SELECT * FROM users WHERE users.id = 1;
+
+-- usersテーブルから「email = xxxxxx」のレコード選択
+SELECT * FROM users WHERE email = "naoko97@fujimoto.com";
+
