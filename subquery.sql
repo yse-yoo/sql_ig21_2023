5ca7d81bba233420dbc0a3c08d3e8647e8743d80
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 12:16:50 +0900

    update

--- subquery.sql
+++ subquery.sql
@@ -3,6 +3,16 @@
 SELECT * FROM user_items
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
 
+SELECT 
+users.name,
+users.gender,
+user_items.amount,
+user_items.total_price,
+user_items.created_at 
+FROM user_items
+JOIN users ON user_items.user_id = users.id
+WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
+
 -- JOINの場合
 SELECT * FROM user_items
 JOIN users ON user_items.user_id = users.id
