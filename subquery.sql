d7bb9705bacc7b89114cde3556a3ce4abe2664f6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 12:08:17 +0900

    update

--- subquery.sql
+++ subquery.sql
@@ -1,4 +1,9 @@
 -- gender = female の購入履歴
+-- Subqueryの場合
 SELECT * FROM user_items
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
 
+-- JOINの場合
+SELECT * FROM user_items
+JOIN users ON user_items.user_id = users.id
+WHERE users.gender = "female";
