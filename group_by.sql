ee87f3bf4f54999c0d96e3eb5b5867d2ef2905c6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 11:12:15 +0900

    update

--- group_by.sql
+++ group_by.sql
@@ -1,3 +1,4 @@
+-- ユーザの集計
 SELECT 
     user_id,
     COUNT(id) AS order_count,
@@ -5,3 +6,11 @@ SELECT
 FROM user_items
 GROUP BY user_id;
 
+-- 商品の集計
+SELECT 
+    item_id,
+    COUNT(id) AS order_count,
+    SUM(amount) AS total_amount,
+    SUM(total_price) AS total_price
+FROM user_items
+GROUP BY item_id;
