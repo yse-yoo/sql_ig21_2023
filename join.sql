f96d3a3ad54e11c1638ed5438e3ab270edadb499
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 10:25:01 +0900

    update

--- join.sql
+++ join.sql
@@ -24,7 +24,7 @@ SELECT
 FROM users
 INNER JOIN user_items ON user_items.user_id = users.id;
 
--- 外部結合
+-- OUTER JOIN(外部結合)
 -- LEFT JOIN
 -- user_items を基準に usersと結合(LEFT)
 SELECT 
@@ -35,3 +35,13 @@ SELECT
     user_items.created_at
 FROM user_items
 LEFT JOIN users ON user_items.user_id = users.id;
+
+-- users を基準に user_itemsと結合(LEFT)
+SELECT 
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM users
+LEFT JOIN user_items ON user_items.user_id = users.id;
