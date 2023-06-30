100260023fa55b0485c9b2b35c0473241903dd78
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 11:53:43 +0900

    update

--- group_by.sql
+++ group_by.sql
@@ -68,4 +68,6 @@ SELECT
 FROM user_items
 JOIN items ON user_items.item_id = items.id
 GROUP BY item_id
-HAVING SUM(user_items.total_price) >= 1000;
+HAVING SUM(user_items.total_price) >= 1000
+ORDER BY user_items.id
+LIMIT 100;
