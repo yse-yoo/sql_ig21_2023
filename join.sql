b43df37c9a2098566432ba3dee615e7a5eb72c7c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-30 10:11:59 +0900

    update

--- join.sql
+++ join.sql
@@ -4,6 +4,7 @@ SELECT
 FROM user_items
 JOIN users ON user_items.user_id = users.id;
 
+-- user_items を基準に usersと結合
 SELECT 
     users.id,
     users.name,
@@ -11,4 +12,14 @@ SELECT
     user_items.amount,
     user_items.created_at
 FROM user_items
-JOIN users ON user_items.user_id = users.id;
+INNER JOIN users ON user_items.user_id = users.id;
+
+-- users を基準に user_itemsと結合
+SELECT 
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM users
+INNER JOIN user_items ON user_items.user_id = users.id;
