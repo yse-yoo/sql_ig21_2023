7384a267a7e46574849d94d7362e1184fa2176f5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-15 11:06:05 +0900

    update

--- java_b.sql
+++ java_b.sql
@@ -9,3 +9,16 @@ FROM reserves
 GROUP BY room_id;
 
 SELECT * FROM rooms WHERE id IN (2, 3, 4);
+
+-- ユーザのレコードを挿入
+INSERT INTO users (name, number) VALUES ('YSE', '00001');
+
+-- user.id = 21
+SELECT id FROM users WHERE number = '000001';
+
+-- rooms.id = 6
+SELECT id FROM rooms WHERE name = '203';
+
+-- 予約のレコードを挿入
+INSERT INTO reserves (user_id, room_id, checkin_at, checkout_at)
+VALUES (21, 6, '2023-07-05 15:00:00', '2023-07-06 10:00:00');
