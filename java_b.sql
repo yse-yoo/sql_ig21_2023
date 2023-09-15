84432764705940df7e790c625957265368f2f23c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-15 10:53:25 +0900

    update

--- java_b.sql
+++ java_b.sql
@@ -1,3 +1,11 @@
 -- 8-1
 SELECT COUNT(id) FROM reserves 
 WHERE checkin_at >= "2023/07/08" AND checkin_at < "2023/07/09";
+
+
+-- 8-2
+SELECT room_id, COUNT(room_id) AS room_id_count 
+FROM reserves 
+GROUP BY room_id;
+
+SELECT * FROM rooms WHERE id IN (2, 3, 4);
